9afe01200ac65a270b5ca304805f7e65b75d7fd1
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2023-11-13 13:54:47 +0100

    Update js.js

--- js.js
+++ js.js
@@ -23,8 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
     flagImg.setAttribute("src", selectedImage);
 
     // Dodavanje posebnog slučaja za ./TZ/index.html
-    if (selectedLang === "fr") {
-      flagImg.setAttribute("src", "./img/icons/fr.svg");
+    if (selectedLang === "en") {
+      flagImg.setAttribute("src", "./img/icons/en.svg");
     }
   }
 
